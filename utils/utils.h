a04f2be542143a6ce601cc24394881fe52479a13
Olivier Guenette <olivierguenette@hotmail.com>
2023-11-10 21:34:59 -0500

    update

--- utils/utils.h
+++ utils/utils.h
@@ -9,8 +9,9 @@ namespace fs = std::filesystem;
 
 Lines read_file(const fs::path &file_path);
 
-Lines &trim(Lines &lines);
 Lines &remove_empty_lines(Lines& lines);
+Lines &trim(Lines &lines);
 std::string &ltrim(std::string &line);
 std::string &rtrim(std::string &line);
+
 
