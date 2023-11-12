6abae071a1546013befeb800e1899df9e4be4925
Olivier Guenette <olivierguenette@hotmail.com>
2023-11-12 09:10:48 -0500

    update repo struct

---
+++ day1/day1.cpp
@@ -0,0 +1,17 @@
+#include "../utils/utils.h"
+
+namespace fs = std::filesystem;
+
+static constexpr const char *file_name = "input.txt";
+
+void first_part(const Lines &lines) {}
+
+void second_part(const Lines &lines) {}
+
+int main()
+{
+  Lines lines = read_file(fs::path(file_name));
+  first_part(lines);
+  second_part(lines);
+  return 0;
+}
