90949769a349dc372fbbe0cfda36111a635ed455
Olivier Guenette <olivierguenette@hotmail.com>
2023-12-01 10:22:57 -0500

    done 2023 day01

--- 2023/day01/day01.cpp
+++ 2023/day01/day01.cpp
@@ -7,7 +7,7 @@ using og::StringList;
 
 static constexpr const char *file_name = "input.txt";
 static constexpr const char *correct_first_part_answer = "54940";
-static constexpr const char *correct_second_part_answer = "Not the right ans";
+static constexpr const char *correct_second_part_answer = "54208";
 
 string first_part(const StringList &lines);
 string second_part(const StringList &lines);
@@ -30,7 +30,6 @@ int main()
 
 string first_part(const StringList &lines)
 {
-  lines.print_lines();
   int sum = 0;
   for (const string& line: lines){
     int first_number = 0;
@@ -40,6 +39,7 @@ string first_part(const StringList &lines)
       const int char_value = i - '0';
       if (char_value >=0 && char_value <= 9){
         first_number = char_value;
+        break;
       }
     }
 
@@ -47,19 +47,101 @@ string first_part(const StringList &lines)
       const int char_value = line.at(i) - '0';
       if (char_value >=0 && char_value <= 9){
         last_number = char_value;
+        break;
       }
     }
-
-
-    std::cout << last_number << first_number << std::endl;
-    string str_value;
-    str_value.append(std::to_string(last_number)).append(std::to_string(first_number));
+    const string str_value (std::to_string(first_number) + std::to_string(last_number));
     sum += std::stoi(str_value);
   }
   return std::to_string(sum);
 }
 
+bool is_digit(char token){
+  const int char_value = token- '0';
+  if (char_value >=0 && char_value <= 9){
+    return true;
+  }
+
+  return false;
+}
+
+int to_digit(const string& token)
+{
+  if (token.find("one") != std::string::npos)
+  {
+    return 1;
+  }
+  if (token.find("two") != std::string::npos)
+  {
+    return 2;
+  }
+  if (token.find("three") != std::string::npos)
+  {
+    return 3;
+  }
+  if (token.find("four") != std::string::npos)
+  {
+    return 4;
+  }
+  if (token.find("five") != std::string::npos)
+  {
+    return 5;
+  }
+  if (token.find("six") != std::string::npos)
+  {
+    return 6;
+  }
+  if (token.find("seven") != std::string::npos)
+  {
+    return 7;
+  }
+  if (token.find("eight") != std::string::npos)
+  {
+    return 8;
+  }
+  if (token.find("nine") != std::string::npos)
+  {
+    return 9;
+  }
+  return 0;
+}
+
 string second_part(const StringList &lines)
 {
-  return "0";
+  int sum = 0;
+  for (const string& line: lines){
+    int first_number = 0;
+    int last_number = 0;
+
+    
+    for (int i = 0; i < line.size() ; i++){
+      if (is_digit(line.at(i))){
+        first_number = line.at(i) - '0';
+        break;
+      }
+
+      const int value = to_digit(line.substr(0, i+1));
+      if (value != 0){
+        first_number = value;
+        break;
+      }
+    }
+
+    for (int i = line.size() -1; i >=0 ; i--){
+      if (is_digit(line.at(i))){
+        last_number = line.at(i) - '0';
+        break;
+      }
+
+      const int value = to_digit(line.substr(i, line.size()-i));
+      if (value != 0){
+        last_number = value;
+        break;
+      }
+     }
+
+    const string str_value (std::to_string(first_number) + std::to_string(last_number));
+    sum += std::stoi(str_value);
+  }
+  return std::to_string(sum);
 }
