b86911ddd7a9b9191a02e8aa5afa3ef26c0b8fc7
Olivier Guenette <olivierguenette@hotmail.com>
2023-12-04 17:42:34 -0500

    done 2023 day04

--- 2023/day04/day04.cpp
+++ 2023/day04/day04.cpp
@@ -14,10 +14,32 @@ using std::string;
 
 static constexpr const char *file_name = "input.txt";
 static constexpr const char *correct_first_part_answer = "20117";
-static constexpr const char *correct_second_part_answer = "Not the right ans";
+static constexpr const char *correct_second_part_answer = "13768818";
 
 string first_part(const StringList &lines);
 string second_part(const StringList &lines);
+
+class Card final
+{
+public:
+  explicit Card(uint64_t index) : m_id(index) {}
+
+  void print() const;
+
+  uint64_t m_id;
+  vector<uint64_t> m_numbers;
+  vector<uint64_t> m_winning_numbers;
+  uint64_t m_count{ 1 };
+};
+
+void Card::print() const
+{
+  std::cout << "Card id: " << m_id << "\n\tNumbers: ";
+  for (auto number : m_numbers) { std::cout << number << "\n"; }
+  std::cout << "\n\tWinning numbers: ";
+  for (auto number : m_winning_numbers) { std::cout << number << "\n"; }
+  std::cout << "\n\tCount: " << m_count << "\n";
+}
 
 int main()
 {
@@ -34,27 +56,6 @@ int main()
 
   return 0;
 }
-
-class Card final
-{
-public:
-  explicit Card(uint64_t index) : m_id(index) {}
-
-  void print() const
-  {
-    printf("Card id: %ld\n", m_id);
-    printf("\tNumbers: ");
-    for (auto number : m_numbers) { printf("%ld ", number); }
-    printf("\n");
-    printf("\tWinning numbers: ");
-    for (auto number : m_winning_numbers) { printf("%ld ", number); }
-    printf("\n");
-  }
-
-  uint64_t m_id;
-  vector<uint64_t> m_numbers;
-  vector<uint64_t> m_winning_numbers;
-};
 
 vector<Card> parse_cards(const StringList &lines)
 {
@@ -85,6 +86,7 @@ vector<Card> parse_cards(const StringList &lines)
     for (const string &number : winning_numbers_str) {
       card.m_winning_numbers.push_back(std::stoll(number));
     }
+
     std::sort(card.m_numbers.begin(), card.m_numbers.end());
     std::sort(card.m_winning_numbers.begin(), card.m_winning_numbers.end());
     cards.push_back(card);
@@ -102,11 +104,11 @@ string first_part(const StringList &lines)
     for (const uint64_t &card_number : card.m_numbers) {
       for (const uint64_t &winning_number : card.m_winning_numbers) {
         if (card_number == winning_number) {
-          if (points == 0U) {
+          if (points == (uint64_t)0) {
             points = 1;
-          } else {
-            points *= 2;
+            break;
           }
+          points *= 2;
           break;
         }
       }
@@ -120,27 +122,30 @@ string first_part(const StringList &lines)
 
 string second_part(const StringList &lines)
 {
-  const vector<Card> cards = parse_cards(lines);
+  vector<Card> cards = parse_cards(lines);
 
-  vector<uint64_t> cards_point;
   for (const auto &card : cards) {
-    uint64_t points = 0;
+    uint64_t number_found = 0;
     for (const uint64_t &card_number : card.m_numbers) {
       for (const uint64_t &winning_number : card.m_winning_numbers) {
         if (card_number == winning_number) {
-          if (points == 0U) {
-            points = 1;
-          } else {
-            points *= 2;
+          number_found++;
+          const auto winning_card = std::find_if(cards.begin(),
+            cards.end(),
+            [&card, &number_found](Card &other_card) {
+              return card.m_id + number_found == other_card.m_id;
+            });
+          if (winning_card != cards.end()) {
+            (*winning_card).m_count += card.m_count;
           }
           break;
         }
       }
     }
-    cards_point.push_back(points);
   }
 
-  return std::to_string(
-    accumulate(cards_point.cbegin(), cards_point.cend(), (uint64_t)0));
-  return "0";
+  return std::to_string(std::accumulate(
+    cards.cbegin(), cards.cend(), 0, [](uint64_t sum, const Card &card) {
+      return sum += card.m_count;
+    }));
 }
